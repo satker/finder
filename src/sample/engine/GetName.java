cbf789c1fcb428eefe76bcc91fcfaf968968d4f9
satker <satker73@gmail.com>
2017-09-03 23:03:47 +0300

    final_var№1

--- src/sample/engine/GetName.java
+++ src/sample/engine/GetName.java
@@ -1,6 +1,7 @@
 package sample.engine;
 
 public class GetName {
+    
     public String getNameFile(String string){
         String[] strings = string.split("\\\\");
         return strings[strings.length-1];
