b8d54bd0171ad2f74b88269319c679e00879d4c7
satker <satker73@gmail.com>
2017-09-05 19:06:27 +0300

    №4

--- src/sample/engine/SearchFiles.java
+++ src/sample/engine/SearchFiles.java
@@ -2,6 +2,7 @@ package sample.engine;
 /*
      Модуль поиска (мозг программы)
  */
+
 import sample.Controller;
 
 import java.io.File;
@@ -9,7 +10,7 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class SearchFiles extends Controller implements Runnable{
+public class SearchFiles extends Controller implements Runnable {
     private String directory; // Искомая директория
     private static String find_text; // Искомый текст
     private static String find_type; // Искомое расширение
@@ -36,59 +37,59 @@ public class SearchFiles extends Controller implements Runnable{
     public void run() {
         File file = new File(directory);
         // Список файлов текущей директории
-        String [] currentFiles = file.list();
+        String[] currentFiles = file.list();
         //если нет файлов ничего не делаем
-        if (currentFiles == null) {
-        }
-        //Если есть, идем дальше и добавляем новую партию в массив
-        else {for (String new_directory:
-                currentFiles) {
-            // Выводим полный путь файла
-            new_directory = getDirectory()+"\\"+new_directory;
-            // Ищем .log файл
-            try{
-
-                // если ничего не ввели выставляем по-умолчанию .log
-                if(find_type.equals("")){
-                    setFind_type("log");
-                }
-                // Если не ввели искомый текст убираем его из условия
-                if(find_text.equals("")){
-                    if(new RegexControl().RegexControl(find_type, new GetName().getNameFile(new_directory))){
-                            res_files.add(new Container(id, new_directory));
-                            id++;
-                // Если все введено проверяем условия
-                }} else {
-                    if(new RegexControl().RegexControl(find_type, new GetName().getNameFile(new_directory))){
-                        if(new ControlSearchText().Control(new_directory, find_text)) {
-                            res_files.add(new Container(id, new_directory));
-                            id++;
+        if (!(currentFiles == null)) {
+            for (String new_directory :
+                    currentFiles) {
+                // Выводим полный путь файла
+                new_directory = getDirectory() + "\\" + new_directory;
+                // Ищем .log файл
+                try {
+                    // Если это файл то проверяем его
+                    if (!(new Control().search(new_directory))) {
+                        // если ничего не ввели выставляем по-умолчанию .log
+                        if (find_type.equals("")) {
+                            setFind_type("log");
                         }
+                        // Если не ввели искомый текст убираем его из условия
+                        if (find_text.equals("")) {
+                            if (new RegexControl().RegexControl(find_type, new GetName().getNameFile(new_directory))) {
+                                res_files.add(new Container(id, new_directory));
+                                id++;
+                                // Если все введено проверяем условия
+                            }
+                        } else {
+                            if (new RegexControl().RegexControl(find_type, new GetName().getNameFile(new_directory))) {
+                                if (new ControlSearchText().Control(new_directory, find_text)) {
+                                    res_files.add(new Container(id, new_directory));
+                                    id++;
+                                }
+                            }
+                        }
+                    } else {
+                        //Создаем пул потоков для рекурсии потоков
+                        ExecutorService threadPool = Executors.newFixedThreadPool(1);
+                        // Создаем и запускаем поток для поиска в директории
+                        SearchFiles recursionThread = new SearchFiles();
+                        recursionThread.setDirectory(new_directory);
+                        threadPool.execute(recursionThread);
+                        // Закрываем потоки
+                        threadPool.shutdown();
+                    }
+                } catch (NullPointerException e1) {
+                    System.out.println("Whats wrong");
+                } catch (IOException e) {
+                    System.out.println("Error");
                 }
-            }}
-            catch (NullPointerException e1){
-                System.out.println("Whats wrong");
             }
-            catch (IOException e){
-                System.out.println("Error");
-            }
-            // Если это дирректория ищем далее, ищем в этой директории
-            if (new Control().search(new_directory)){
-                //Создаем пул потоков для рекурсии потоков
-                ExecutorService threadPool = Executors.newFixedThreadPool(1);
-                // Создаем и запускаем поток для поиска в директории
-                SearchFiles recursionThread = new SearchFiles();
-                recursionThread.setDirectory(new_directory);
-                threadPool.execute(recursionThread);
-                // Закрываем потоки
-                threadPool.shutdown();
-            }
-        }}
+        }
     }
 }
+
 // Проверка директория ли это
 class Control extends SearchFiles {
-    Boolean search(String s){
+    Boolean search(String s) {
         File name = new File(s);
         return name.isDirectory();
     }
