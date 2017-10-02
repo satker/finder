b27ebbf6838654c23cce369760edadd4bb5ce3f8
satker <satker73@gmail.com>
2017-10-02 17:43:06 +0300

    final_version

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,29 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 import sample.engine.Container;
 import sample.engine.SearchFiles;
 
+import javax.swing.event.AncestorEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -21,7 +34,7 @@ import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-interface OpenFile{
+interface OpenFile {
     LinkedList File(String str) throws IOException;
 }
 
@@ -42,6 +55,9 @@ public class Controller extends Container implements Initializable {
     private TextField inner_finder;
 
     @FXML
+    private TextArea text_output_file;
+
+    @FXML
     private TextField what_find;
 
     @FXML
@@ -53,7 +69,7 @@ public class Controller extends Container implements Initializable {
     private static String choose_res;
 
     @FXML
-    private void startFind(ActionEvent actionEvent){
+    private void startFind(ActionEvent actionEvent) {
         SearchFiles mem_find = new SearchFiles(inner_finder.getText(), what_find_text.getText(), what_find.getText());
         ExecutorService pool = Executors.newCachedThreadPool();
         pool.execute(mem_find);
@@ -64,12 +80,12 @@ public class Controller extends Container implements Initializable {
         result_finder.setRowFactory(tv -> {
             TableRow<Container> row = new TableRow<>();
             row.setOnMouseClicked(event -> {
-                if (! row.isEmpty() && event.getButton()== MouseButton.PRIMARY
+                if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY
                         && event.getClickCount() == 1) {
-                            Container clickedRow = row.getItem();
-                            choose_res = clickedRow.getName();
-                            /////// Вывод имени
-                            change_name.setText(new File(choose_res).getName());
+                    Container clickedRow = row.getItem();
+                    choose_res = clickedRow.getName();
+                    /////// Вывод имени
+                    change_name.setText(new File(choose_res).getName());
                 }
             });
             return row;
@@ -77,37 +93,37 @@ public class Controller extends Container implements Initializable {
     }
 
     @FXML
-    private void openFile(){
+    private void openFile() {
         try {
             OpenFile open_file_text = (str) -> {
                 File file = new File(str);
                 BufferedReader fin = new BufferedReader(new FileReader(file));
                 LinkedList<String> linkedList = new LinkedList<>();
                 String line;
-                while((line = fin.readLine()) != null) {
+                while ((line = fin.readLine()) != null) {
                     linkedList.add(line);
                 }
                 return linkedList;
             };
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-
-            alert.setTitle("Information");
-            alert.setHeaderText(null);
             /////// Вывод содержания
             String str_final = "";
             for (Object i : open_file_text.File(choose_res)) {
-                str_final += "\n" + i.toString();
+                str_final = str_final.concat("\n" + i.toString());
             }
-            alert.setContentText(str_final);
-            alert.showAndWait();
+            text_output_file.textProperty().addListener((ChangeListener<Object>) (observable, oldValue, newValue) -> {
+                text_output_file.setScrollTop(Double.MAX_VALUE); //this will scroll to the bottom
+                //use Double.MIN_VALUE to scroll to the top
+            });
+            text_output_file.setText(str_final);
+            text_output_file.appendText("");
             /////////
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("File not Found");
         }
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
 
     }
 }
--- src/sample/engine/SearchFiles.java
+++ src/sample/engine/SearchFiles.java
@@ -5,17 +5,17 @@ package sample.engine;
 
 import sample.Controller;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
+// Функциональный интерфейс для проверки файлов
 interface Modules<T> {
     boolean Control(T what, T testString) throws IOException;
 }
@@ -30,29 +30,21 @@ public class SearchFiles extends Controller implements Runnable {
     private volatile String directory; // Искомая директория
     private static String find_text; // Искомый текст
     private static String find_type; // Искомое расширение
-    private static volatile int id = 1; // ID результата
+    private static int id = 1; // ID результата
 
     @Override
     public void run() {
-        try {
-            LinkedList<String> res_set;
-            LinkedList<String> mem_for_remove = new LinkedList<>();
-            res_set = current_Files(directory);
-            while (res_set.size() > 0) {
-                mem_for_remove.clear();
-                // Параллельные стримы для перебора коллекции
-                res_set.parallelStream().forEach(s -> {
-                    try {
-                        res_set.addAll(current_Files(s));
-                        mem_for_remove.add(s);
-                    } catch (IOException exp){
-                        System.out.println();
-                    }
-                });
-                res_set.removeAll(mem_for_remove);
-            }
-        } catch (IOException e) {
-            System.out.println("error");
+        LinkedList<String> res_set;
+        LinkedList<String> mem_for_remove = new LinkedList<>();
+        res_set = current_Files(directory);
+        while (res_set.size() > 0) {
+            mem_for_remove.clear();
+            // Параллельные стримы для перебора коллекции
+            res_set.parallelStream().forEach(s -> {
+                res_set.addAll(current_Files(s));
+                mem_for_remove.add(s);
+            });
+            res_set.removeAll(mem_for_remove);
         }
     }
 
@@ -69,15 +61,7 @@ public class SearchFiles extends Controller implements Runnable {
         // Проверка наличия искомого текста в файле
         Modules<String> control_text = (file_name, find_text) -> {
             Stream<String> stream = Files.lines(Paths.get(file_name));
-            //File file = new File(file_name);
-            //BufferedReader fin = new BufferedReader(new FileReader(file));
-            //String line;
-            //while ((line = fin.readLine()) != null) {
-            //    if (line.contains(find_text)) {
-            //        result_bool = true;
-            //    }
-            //}
-            return stream.anyMatch((s)->s.contains(find_text));
+            return stream.anyMatch((s) -> s.contains(find_text));
         };
         // если ничего не ввели выставляем по-умолчанию .log
         if (find_type.equals("")) {
@@ -90,7 +74,7 @@ public class SearchFiles extends Controller implements Runnable {
             }
         } else {
             if (control_file_type.Control(find_type, text)) {
-                if(control_text.Control(new_directory, find_text)) {
+                if (control_text.Control(new_directory, find_text)) {
                     result_find = new_directory;
                 }
             }
@@ -98,26 +82,28 @@ public class SearchFiles extends Controller implements Runnable {
         return result_find;
     }
 
-    private synchronized LinkedList<String> current_Files(String string) throws IOException {
+    private synchronized LinkedList<String> current_Files(String string) {
         LinkedList<String> linkedList = new LinkedList<>();
         File file = new File(string);
         // Список файлов текущей директории
         String[] currentFiles = file.list();
         if (currentFiles != null) {
-            for (String currentFile : currentFiles) {
-                String add_mem = string + "\\" + currentFile;
-                file = new File(add_mem);
-                if (currentFile != null) {
-                    // Если файл делаем сразу проверку
-                    if (file.isFile() && !(add_files(add_mem) == null) && file.canRead()) {
-                        res_files.add(new Container(id, add_files(add_mem)));
-                        id++;
-                    }//////// Если каталог записываем в колекцию и продолжаем поиск
-                    else {
-                        linkedList.add(add_mem);
-                    }
-                }
-            }
+            Arrays.stream(currentFiles).map(i -> string + "\\" + i)
+                    .forEach(i -> {
+                        try {
+                            // Если файл делаем сразу проверку
+                            if (new File(i).isFile() && !(add_files(i) == null) && new File(i).canRead()) {
+                                res_files.add(new Container(id, add_files(i)));
+                                id++;
+                            }
+                            // Если каталог записываем в колекцию и продолжаем поиск
+                            else {
+                                linkedList.add(i);
+                            }
+                        } catch (IOException e) {
+                            System.out.println("error");
+                        }
+                    });
         }
         return linkedList;
     }
