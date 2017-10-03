e3ac3e2e6b68c89871ccb3c11f000e3b222ed71b
satker <satker73@gmail.com>
2017-10-03 21:58:40 +0300

    production
    version 0.7

--- src/sample/engine/SearchFiles.java
+++ src/sample/engine/SearchFiles.java
@@ -17,8 +17,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 // Функциональный интерфейс для проверки файлов
-interface Modules<T> {
-    boolean Control(T what, T testString) throws IOException;
+interface Modules {
+    boolean control(String what, String testString) throws IOException;
 }
 
 public class SearchFiles extends Controller implements Runnable {
@@ -33,42 +33,34 @@ public class SearchFiles extends Controller implements Runnable {
     private static String find_type; // Искомое расширение
     private static int id = 1; // ID результата
 
+    // Проверка расширения
+    private Modules control_file_type = (what, testString) -> {
+        Pattern p = Pattern.compile(".+\\." + what + "$");
+        Matcher m = p.matcher(testString);
+        return m.matches();
+    };
+    // Проверка наличия искомого текста в файле
+    private Modules control_text = (file_name, find_text) -> {
+        Stream<String> stream = Files.lines(Paths.get(file_name));
+        return stream.anyMatch(s -> s.contains(find_text));
+    };
+
     @Override
     public void run() {
         directory = directory.equals("") ? "C:\\" : directory;
         find_type = find_type.equals("") ? "log" : find_type; // если ничего не ввели выставляем по-умолчанию .log
-        LinkedList<String> res_set = current_Files(directory);
+        LinkedList<String> res_set = currentDirectories(directory);
         LinkedList<String> mem_for_add = new LinkedList<>();
         while (res_set.size() > 0) {
             // Параллельные стримы для перебора коллекции
-            res_set.parallelStream().filter(Objects::nonNull).forEach(s ->
-                    mem_for_add.addAll(current_Files(s))
-            );
+            res_set.parallelStream().filter(Objects::nonNull).forEach(s -> mem_for_add.addAll(currentDirectories(s)));
             res_set.clear();
             res_set.addAll(mem_for_add);
             mem_for_add.clear();
         }
     }
-
-    private synchronized String add_files(String new_directory) throws IOException {
-        // Проверка файла и его пути на искомое расширение
-        Modules<String> control_file_type = (what, testString) -> {
-            Pattern p = Pattern.compile(".+\\." + what + "$");
-            Matcher m = p.matcher(testString);
-            return m.matches();
-        };
-        // Проверка наличия искомого текста в файле
-        Modules<String> control_text = (file_name, find_text) -> {
-            Stream<String> stream = Files.lines(Paths.get(file_name));
-            return stream.anyMatch(s -> s.contains(find_text));
-        };
-        return control_file_type.Control(find_type, new File(new_directory).getName()) &&
-                (find_text.equals("") || control_text.Control(new_directory, find_text)) ?
-                new_directory :
-                null;
-    }
-
-    private synchronized LinkedList<String> current_Files(String string) {
+    // Возвращает список директорий в папке
+    private synchronized LinkedList<String> currentDirectories(String string) {
         LinkedList<String> linkedList = new LinkedList<>();
         // Список файлов текущей директории
         String[] currentFiles = new File(string).list();
@@ -76,10 +68,13 @@ public class SearchFiles extends Controller implements Runnable {
             Arrays.stream(currentFiles).filter(Objects::nonNull).map(i -> string + "\\" + i)
                     .forEach(i -> {
                         try {
-                            String add_f = add_files(i);
+                            File file = new File(i);
+                            String add_file = (control_file_type.control(find_type, file.getName()) &&
+                                    (find_text.equals("") || control_text.control(i, find_text))) ?
+                                    i : null;
                             // Если файл делаем сразу проверку
-                            if (new File(i).isFile() && !(add_f == null) && new File(i).canRead()) {
-                                res_files.add(new Container(id, add_f));
+                            if (file.isFile() && add_file != null && file.canRead()) {
+                                res_files.add(new Container(id, add_file));
                                 id++;
                             }
                             // Если каталог записываем в колекцию и продолжаем поиск
