6b15b9a0ebb267acad7e8876cd0a92f003a1d47f
satker <satker73@gmail.com>
2017-10-03 13:01:39 +0300

    production
    version 0.4

--- src/sample/engine/SearchFiles.java
+++ src/sample/engine/SearchFiles.java
@@ -11,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -27,7 +28,7 @@ public class SearchFiles extends Controller implements Runnable {
         SearchFiles.find_type = find_type;
     }
 
-    private volatile String directory; // Искомая директория
+    private String directory; // Искомая директория
     private static String find_text; // Искомый текст
     private static String find_type; // Искомое расширение
     private static int id = 1; // ID результата
@@ -36,21 +37,19 @@ public class SearchFiles extends Controller implements Runnable {
     public void run() {
         LinkedList<String> res_set;
         LinkedList<String> mem_for_add = new LinkedList<>();
-        if (directory.equals("")) {
-            directory = "C:\\";
-        }
+        directory = directory.equals("") ? "C:\\" : directory;
+        find_type = find_type.equals("") ? "log" : find_type; // если ничего не ввели выставляем по-умолчанию .log
         res_set = current_Files(directory);
         while (res_set.size() > 0) {
             mem_for_add.clear();
             // Параллельные стримы для перебора коллекции
-            res_set.parallelStream().forEach(s ->
+            res_set.parallelStream().filter(Objects::nonNull).forEach(s ->
                     mem_for_add.addAll(current_Files(s))
             );
             res_set.clear();
             res_set.addAll(mem_for_add);
         }
     }
-
 
     private synchronized String add_files(String new_directory) throws IOException {
         String result_find = null;
@@ -64,23 +63,14 @@ public class SearchFiles extends Controller implements Runnable {
         // Проверка наличия искомого текста в файле
         Modules<String> control_text = (file_name, find_text) -> {
             Stream<String> stream = Files.lines(Paths.get(file_name));
-            return stream.anyMatch((s) -> s.contains(find_text));
+            return stream.anyMatch(s -> s.contains(find_text));
         };
-        // если ничего не ввели выставляем по-умолчанию .log
-        if (find_type.equals("")) {
-            find_type = "log";
-        }
-        // Если не ввели искомый текст убираем его из условия
-        if (find_text.equals("")) {
-            if (control_file_type.Control(find_type, text)) {
-                result_find = new_directory;
-            }
-        } else {
-            if (control_file_type.Control(find_type, text)) {
-                if (control_text.Control(new_directory, find_text)) {
-                    result_find = new_directory;
-                }
-            }
+        if (control_file_type.Control(find_type, text)) {
+            result_find = find_text.equals("") ?
+                    new_directory :
+                    (control_text.Control(new_directory, find_text) ?
+                            new_directory :
+                            null);
         }
         return result_find;
     }
